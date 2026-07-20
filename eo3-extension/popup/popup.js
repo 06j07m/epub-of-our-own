0466276009b9e4950aff52abdcaf66db67193222
Mona <itsaysusernamenotallowed@gmail.com>
2026-07-20 17:06:25 -0400

    let popup inject script to find download link

--- eo3-extension/popup/popup.js
+++ eo3-extension/popup/popup.js
@@ -1,9 +1,20 @@
 const urlPrefix = "https://archiveofourown.org/works/";
 
+function testFunction(tab) {
+    // #main > div.work > ul > li.download > ul > li:nth-child(5) > a
+    const link = document.querySelector("#main > div.work > ul > li.download > ul > li:nth-child(5) > a");
+    const msg = link ? link.href : "Not foudn";
+    alert(msg);
+}
+
+
 document.getElementById("btn").addEventListener("click", () => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         if (tabs[0].url.startsWith(urlPrefix)) {
-            document.getElementById("output").textContent = "The extension can be used on this page.";
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                func: testFunction
+            })
         } else {
             document.getElementById("output").textContent = "The extension cannot be used on this page.";
         }
